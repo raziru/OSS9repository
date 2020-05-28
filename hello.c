581afeb3382b9010537a10b985ae0f50e02ade6f
MinKyuLee <himkmk@naver.com>
2020-05-28 20:03:24 +0900

    minkyulee

--- hello.c
+++ hello.c
@@ -5,5 +5,5 @@ int main()
     printf("chage 3 time 2016310464 Yoon hyeongho \n");
     printf("hello\n");
     printf("hello world \n");
-    
+    printf("hello this is minkyulee 2017314789 \n");
 }
